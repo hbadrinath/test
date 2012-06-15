291b927e1850c80613ec643fb53ac4f9766182fe
worker <worker@sekon.local>
2012-06-15 20:38:47 +0530

    Removed green

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Test repo for setting up jenkin CI system
 ====
 
 To build egg: python setup.py bdist_egg 
+
 To run tests: python setup.py nosetests
  
--- testme/test.py
+++ testme/test.py
@@ -1,4 +1,5 @@
 #/usr/bin/env python
+import nose
 
 def grt_wrld():
 	return "Hello World!!" 
@@ -7,6 +8,9 @@ def test_grtwrld():
 	assert grt_wrld() == "Hello World!!" 
 	assert __name__ != '__main__'
 
+#def test_all():
+#	print "SKN SKN SKN SKN SKN SKN SKN SKN SKN SKN SKN SKN"
+#	nose.main()
+
 if __name__ == '__main__':
 	print "[Error] cant run directly from command line"
-
